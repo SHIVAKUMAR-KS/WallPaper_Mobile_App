ed9b87f93aad05731e4d8eb47224b1767b2d93d2
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2024-10-11 21:17:23 +0530

    Update account.tsx

--- app/account.tsx
+++ app/account.tsx
@@ -3,7 +3,7 @@ import { Text,View } from "react-native";
 export default function account(){
 
     return <View>
-        <Text>Account page</Text>
+        <Text>Account pages</Text>
     </View>
 
 }
