c4b4d00202c628a896fc015c0d483091506997e9
shiva kumar <115865333+SHIVAKUMAR-KS@users.noreply.github.com>
2024-10-11 21:17:59 +0530

    Update explore.tsx

--- app/explore.tsx
+++ app/explore.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function explore(){
 
     return <View>
-        <Text>Explore page</Text>
+        <Text>Explore pages</Text>
     </View>
 
 }
