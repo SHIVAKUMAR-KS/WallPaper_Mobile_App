42323228a50b13d9a9a2a98ca3236c5574aecd7e
SHIVAKUMAR-KS <kshiva09283@gmail.com>
2024-10-14 06:29:06 +0530

    done with checkpoint 5

--- INC.md
+++ INC.md
@@ -10,3 +10,11 @@ safeAreaView
 ```
 
 ### layout iis very important to render any specific pages
+
+### checkpoint 5
+using react navigation package
+```
+https://reactnavigation.org/docs/material-top-tab-navigator
+```
+
+
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,32 @@
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, Image, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function(){
-  const [count,setCount]=useState(0);
+  // const [count,setCount]=useState(0);
 
-  useEffect(()=>{
-    let interval=setInterval(() => {
-      setCount(c=>c+1)
-    }, 1000)
-    return ()=>{
-      clearInterval(interval)
-    }
-  },[])
+  // useEffect(()=>{
+  //   let interval=setInterval(() => {
+  //     setCount(c=>c+1)
+  //   }, 1000)
+  //   return ()=>{
+  //     clearInterval(interval)
+  //   }
+  // },[])
 
   return <SafeAreaView>
-    <Text>Index page</Text>
-   
+    <Link href={'/library'}>
+        <Text>page 1</Text>
+    </Link>
+    <Link href={'/liked'}>
+        <Text>page 2</Text>
+    </Link>
+    <Link href={'/suggested'}>
+        <Text>page 3</Text>
+    </Link>
+    <Text>For You Page</Text>
+  
   </SafeAreaView>
 }
